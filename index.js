778f91d6d507bb01ae80553b984c0f17fbd6af2d
Michael Moravetz <moravemv@gmail.com>
2018-12-03 21:40:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+return string.toUpperCase ()
+}
+function whisper(string){
+  return string.toLowerCase ()
+}
+function logShout(string) {
+ console.log(string.toUpperCase ())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase ())
+}
+function sayHiToGrandma(string) {
+  var lowercase=(string)
+  return ("I can\'t hear you!")
+}
+function sayHiToGrandma(string) {
+  var uppercase=(string)
+  return ("YES INDEED!")
+}
+function sayHiToGrandma(string) {
+  
+}
